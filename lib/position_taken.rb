cdf68821a200bdcdbf886c1a455a9e51ffc51374
mattwecht <mattwecht@gmail.com>
2018-05-23 15:56:39 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,4 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] !=" " || board[index] != "" || board[index] != nil
-    return true
-  else
-    return false
-  end
+  board[index] !=" "
 end
